854b55c23aeedd21946efd8f0eb8a204765888fa
GuestJim <mlcforeigner@gmail.com>
2022-08-23 09:10:10 -0400

    Improved graph with facets and title

--- Force Curve Graphs.r
+++ Force Curve Graphs.r
@@ -8,6 +8,10 @@ CSVsHR	=	list.files(recursive = TRUE, pattern = "*RAW.csv")
 
 SELECT	=	1
 
+simpsplit	=	function(...)	unlist(strsplit(...))
+
+NAME	=	simpsplit(CSVs[1], "/")[1]
+
 TEXT	=	readLines(CSVs[SELECT],	warn = FALSE,	n = 100)
 for (LINE in 1:length(TEXT))	{	if (TEXT[LINE] == "No.,Force,Unit,Displacement,Unit,Judge,Position,Time,Date,")	break	}
 
@@ -19,8 +23,12 @@ BOT	=	ceiling(median(which(DATA$Displacement == max(DATA$Displacement))))
 DATA$Stroke	=	ordered(ifelse(1:nrow(DATA) < BOT, "Down", "Up"), c("Down", "Up"))
 DATA$Reflect	=	ifelse(DATA$Stroke == "Down", DATA$Displacement, -DATA$Displacement)
 
+STROKEcolor	=	scale_color_manual(values = c("Down" = "black", "Up" = "grey"), guide = "none")
+
 ggplot(DATA[DATA$Force != 0, ], aes(x = Reflect, y = Force, color = Stroke)) + 
+ggtitle(paste0(NAME, " Force Curve")) + 
 geom_vline(xintercept = 0) + geom_hline(yintercept = 0) + 
-geom_point() + 
+geom_point() + STROKEcolor + 
 scale_x_continuous(name	=	"Displacement (mm)",	breaks	=	-5:5) +
-scale_y_continuous(name	=	"Force (gf)",			breaks	=	seq(0, 1000, by = 20))
+scale_y_continuous(name	=	"Force (gf)",			breaks	=	seq(0, 1000, by = 20)) +
+facet_grid(cols = vars(Stroke), scales = "free_x")
