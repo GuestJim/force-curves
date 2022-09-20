d322c64ffc5e0c21a0a6f1b148535dd0c4f42329
GuestJim <mlcforeigner@gmail.com>
2022-09-20 16:16:31 -0400

    Better button label

--- app_UI.r
+++ app_UI.r
@@ -9,7 +9,7 @@ selectionUI	<-	function(id)	{
 			selectInput(pasteID(),		label = "Switch Selection",	choices = FILES,	size = 10,	multiple = TRUE,	selectize = FALSE),
 			selectizeInput(pasteID("IZE"),		label = "Switch Search",	choices = FILES,	multiple = TRUE),
 			actionButton(pasteID("Clear"),	label = "Clear Selection"),
-			actionButton(pasteID("Apply"),	label = "Apply Selection to Overlay"),
+			actionButton(pasteID("Apply"),	label = "Apply Selection to Other Tab"),
 			hr(),
 			bookmarkButton(label = "Bookmark Switch Selection",	title = "Bookmark Switch Selection"),
 			a("ThereminGoat.com",		href="https://www.theremingoat.com/"),
